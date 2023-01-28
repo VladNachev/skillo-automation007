80692bada90b8c319c7d164ffa9507bfeaa02bee
Vladimir Nachev <nachev.vlad@gmail.com>
2023-01-28 15:15:48 +0200

    add reverser iteration

---
+++ lecture3/reverseIteration.java
@@ -0,0 +1,10 @@
+package lecture3;
+
+public class reverseIteration {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        for (int i = numbers.length - 1; i > 0; i--){
+            System.out.println(numbers[i]);
+        }
+    }
+}
